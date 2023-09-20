c0f166992bdd2b03a9c4a852d4914dbc4aba74d4
trkpetra <torok.petra.7@gmail.com>
2023-09-20 15:07:50 +0200

    Create builtin_wrappers.py

---
+++ src/utils/builtin_wrappers.py
@@ -0,0 +1,11 @@
+
+def number_of_elements_in_list(input_list):
+    return len(input_list)
+
+
+def remove_every_element_from_list(input_list):
+    input_list.clear()
+
+
+def reverse_list(input_list):
+    return input_list.reverse()
